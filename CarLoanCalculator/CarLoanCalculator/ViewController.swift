0f9f38c90cab224ce2b4b58f1623765d795f816a
Francis Chary <fchary@gmail.com>
2014-08-09 00:28:12 -0600

    Added the base project

---
+++ CarLoanCalculator/CarLoanCalculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  CarLoanCalculator
+//
+//  Created by Francis Chary on 2014-08-08.
+//  Copyright (c) 2014 Churowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var monthsSlider:UISlider?;
+    @IBOutlet var paymentText:UILabel?;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
